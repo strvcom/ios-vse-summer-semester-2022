4899bb258ea809cafc3c0318e0055ef34fcf9667
Tomas Cejka <ceja@centrum.cz>
2022-03-03 11:30:16 +0100

    [feat] add main TabView, NavigationViews for every tab

--- RickAndMorty/RickAndMorty/Scenes/ContentView.swift
+++ RickAndMorty/RickAndMorty/Scenes/ContentView.swift
@@ -9,8 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello VSE!!!")
-            .font(.appItemLargeTitle)
+        TabView {
+            NavigationView {
+                Text("CHARACTERS")
+                    .font(.appItemLargeTitle)
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .tabItem {
+                Image(systemName: "person.2")
+                
+                Text("Characters")
+            }
+            
+            NavigationView {
+                Text("EPISODES")
+                    .font(.appItemLargeTitle)
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .tabItem {
+                Image(systemName: "film")
+                
+                Text("Episodes")
+            }
+            
+            NavigationView {
+                Text("LOCATIONS")
+                    .font(.appItemLargeTitle)
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .tabItem {
+                Image(systemName: "globe.europe.africa")
+                
+                Text("Locations")
+            }
+            
+            NavigationView {
+                Text("LIKES")
+                    .font(.appItemLargeTitle)
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .tabItem {
+                Image(systemName: "heart.text.square")
+                
+                Text("Likes")
+            }
+        }
+        .foregroundColor(.appTextBody)
+        .preferredColorScheme(.none)
     }
 }
 
