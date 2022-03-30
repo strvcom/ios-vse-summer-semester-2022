a74baf4fc842f4cfdf0919dc9f70122c9a451eeb
Veronika Zelinkova <vzelinkova@email.cz>
2022-03-30 13:03:52 +0200

    Add LoginScreen (#3)

---
+++ LoginScreen/LoginScreen/Styles/Color+App.swift
@@ -0,0 +1,15 @@
+//
+//  Color+App.swift
+//  RickAndMorty
+//
+//  Created by Jan Kaltoun on 29.01.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static let appBackground = UIColor(named: "Colors/background")
+    static let appText = UIColor(named: "Colors/text")
+    static let appButtonBackground = UIColor(named: "Colors/buttonBackground")
+    static let appAction = UIColor(named: "Colors/action")
+}
